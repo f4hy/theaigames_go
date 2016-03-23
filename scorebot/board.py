a3827203b01247f53017b6f6f14a4685ecb17ef7
Brendan Fahy <bmfahy@gmail.com>
2016-03-23 16:56:26 +0900

    added flood fill aglos

--- scorebot/board.py
+++ scorebot/board.py
@@ -1,5 +1,4 @@
 import logging
-import sb_logic
 from pprint import pprint_field
 from collections import deque
 from collections import defaultdict
@@ -16,14 +15,23 @@ class Board(object):
         self.h = h
 
     def setboard(self, fstr):
+        old = self.field
+        died = {1: [], 2: []}
         flist = deque(fstr.replace(';', ',').split(','))
         # logging.info("setting board using {}".format(flist))
         for y in range(self.h):
             for x in range(self.w):
-                self.field[(x,y)] = int(flist.popleft())
+                new = int(flist.popleft())
+                if old[(x,y)] in [1,2] and new == 0:
+                    logging.info("{} {} DIED!!!".format(x,y))
+                    died[old[(x,y)]].append((x,y))
+                self.field[(x,y)] = new
         # logging.info("field {}".format(self.field))
         # logging.info("board \n{}".format(pprint_field(self.field)))
+        return died
 
+    def fieldstring(self):
+        return ",".join(str(self.field[(x,y)]) for y in range(self.h)for x in range(self.w) )
 
     def liberties(self, x, y, ID):
         liberty = 0
@@ -31,6 +39,46 @@ class Board(object):
             if self.field[i] == 0 or self.field[i] == ID:
                 liberty +=1
         return liberty
+
+    def fill_liberties(self, x, y):
+        target = self.field[(x,y)]
+        checked = []
+        liberties = []
+        filled = [(x,y)]
+        dq = deque(stensils.plus((x,y)))
+
+        while len(dq) > 0:
+            current = dq.pop()
+            checked.append(current)
+            if self.field[current] == target:
+                filled.append(current)
+                dq.extend(i for i in stensils.plus(current) if i not in checked)
+            if self.field[current] == 0:
+                liberties.append(current)
+        return filled, liberties
+
+    def owned(self, x, y):
+
+        target = 0
+        checked = []
+        edge = None
+        filled = [(x,y)]
+        dq = deque(stensils.plus((x,y)))
+
+        while len(dq) > 0:
+            current = dq.pop()
+            checked.append(current)
+            if self.field[current] == target:
+                filled.append(current)
+                dq.extend(i for i in stensils.plus(current) if i not in checked)
+            if self.field[current] != 0:
+                if edge is None:
+                    edge = self.field[current]
+                else:
+                    if edge != self.field[current]:
+                        return None
+        return edge
+
 
 
     def is_legal(self, x, y, ID):
